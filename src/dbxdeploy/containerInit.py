64a4235029d7485e8a5e14ad50fb68a8b0fef495
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-04 16:43:13 +0100

    Updated for pyfony 0.7.*

--- src/dbxdeploy/containerInit.py
+++ src/dbxdeploy/containerInit.py
@@ -1,24 +1,17 @@
-from consolebundle.ConsoleBundle import ConsoleBundle
 from injecta.config.YamlConfigReader import YamlConfigReader
 from injecta.container.ContainerInterface import ContainerInterface
 from injecta.package.pathResolver import resolvePath
-from typing import List
-from pyfony.kernel.BaseKernel import BaseKernel
-from pyfonybundles.Bundle import Bundle
+from pyfony.Kernel import Kernel
+from pyfonybundles.loader import pyfonyBundlesLoader
 from dbxdeploy.DbxDeployBundle import DbxDeployBundle
 
 def initContainer(appEnv) -> ContainerInterface:
-    class Kernel(BaseKernel):
-
-        def _registerBundles(self) -> List[Bundle]:
-            return [
-                ConsoleBundle(),
-                DbxDeployBundle()
-            ]
+    bundles = [*pyfonyBundlesLoader.loadBundles(), DbxDeployBundle.autodetect()]
 
     kernel = Kernel(
         appEnv,
         resolvePath('dbxdeploy') + '/_config',
+        bundles,
         YamlConfigReader()
     )
 
